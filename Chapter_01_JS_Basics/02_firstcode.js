f705e199bbaa4da36136dff2fec8105b62879e52
greeshma <p.greeshma23@gmail.com>
2026-07-09 17:33:06 +0530

    Initial javascript code

---
+++ Chapter_01_JS_Basics/02_firstcode.js
@@ -0,0 +1,13 @@
+let a=10;
+console.log(a);
+console.log("a is:",a);
+
+for(let i=0;i<5;i++){
+    console.log("i is:",i);
+    Payment(a+i);
+}
+
+function Payment(a){
+    console.log("amount is :",a);
+
+}
